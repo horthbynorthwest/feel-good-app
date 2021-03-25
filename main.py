232350fa3f780a60c7f30204366756be921f570d
Georgie Horth <georgiehorth@gmail.com>
2021-03-25 14:04:41 +0000

    Sign up button now adds a user to the json file (doesn't save it) and prints out the new dictionary of info

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen 
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
@@ -12,7 +14,10 @@ class SignUpScreen(Screen):
    def add_user(self, uname, pword):
        with open("users.json") as file:
            users = json.load(file)
-        print(users)
+       print(users)
+       users[uname] = {'username': uname, 'password': pword,
+        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+       print(users)
 
 class RootWidget(ScreenManager):
     pass
